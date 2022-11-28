534a5d4a31be0309927bce37021aa1e0af1ac0b5
nnghi235 <104557843+nnghi235@users.noreply.github.com>
2022-11-28 21:40:32 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,107 @@
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
+read -p " NODE ID 1: " node_id1
+  [ -z "${node_id1}" ] && node_id1=0
+  
+read -p " NODE ID 2: " node_id2
+  [ -z "${node_id2}" ] && node_id2=0
+rm -rf /etc/XrayR/speed4g.crt
+rm -rf /etc/XrayR/speed4g.key
+openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /etc/XrayR/speed4g.crt -keyout /etc/XrayR/speed4g.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
+cd /etc/XrayR
+cat >config.yml <<EOF
+Log:
+  Log:
+  Level: none 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json
+InboundConfigPath: # /etc/XrayR/custom_inbound.json
+RouteConfigPath: # /etc/XrayR/route.json
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json
+ConnetionConfig:
+  Handshake: 4 
+  ConnIdle: 30 
+  UplinkOnly: 2 
+  DownlinkOnly: 4 
+  BufferSize: 64 
+Nodes:
+  -
+    PanelType: "V2board" 
+    ApiConfig:
+      ApiHost: "https://tltvpn.xyz"
+      ApiKey: "tanlaotabfsjcjsj23"
+      NodeID1: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0
+      DeviceLimit: 5
+      RuleListPath: # /etc/XrayR/rulelist
+    ControllerConfig:
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
+        -
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
+      CertConfig:
+        CertMode: file 
+        CertDomain: "vn.speed4g.me" 
+        CertFile: /etc/XrayR/speed4g.crt
+        KeyFile: /etc/XrayR/speed4g.key
+        Provider: cloudflare 
+        Email: test@me.com
+        DNSEnv: 
+          CLOUDFLARE_EMAIL:
+          CLOUDFLARE_API_KEY:
+  -
+    PanelType: "V2board" 
+    ApiConfig:
+      ApiHost: "https://tltvpn.xyz"
+      ApiKey: "tanlaotabfsjcjsj23"
+      NodeID2: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0
+      DeviceLimit: 5
+      RuleListPath: # /etc/XrayR/rulelist
+    ControllerConfig:
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
+        -
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
+      CertConfig:
+        CertMode: file 
+        CertDomain: "vn.speed4g.me" 
+        CertFile: /etc/XrayR/speed4g.crt 
+        KeyFile: /etc/XrayR/speed4g.key
+        Provider: cloudflare 
+        Email: test@me.com
+        DNSEnv: 
+          CLOUDFLARE_EMAIL: 
+          CLOUDFLARE_API_KEY: 
+EOF
+sed -i "s|NodeID1:.*|NodeID: ${node_id1}|" ./config.yml
+sed -i "s|NodeID2:.*|NodeID: ${node_id2}|" ./config.yml
+cd /root && xrayr restart && echo -e "   Cài Đặt Hoàn Tất!"
